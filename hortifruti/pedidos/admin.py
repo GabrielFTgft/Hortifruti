0f162ccc4c86583d6049d728e0d0f8e32f59e1bb
Gabriel Teixeira <gabrielft8gft@gmail.com>
2024-12-02 16:36:50 -0300

    Alteracao em Pedido

--- hortifruti/pedidos/admin.py
+++ hortifruti/pedidos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+# Register your models here.
+from .models import Pedido
+from .models import ItemPedido
+
+admin.site.register(Pedido)
+admin.site.register(ItemPedido)
--- hortifruti/pedidos/models.py
+++ hortifruti/pedidos/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from usuarios.models import Cliente
 from produtos.models import Produto
 
 class Pedido(models.Model):
-    cliente = models.ForeignKey(User, on_delete=models.CASCADE)  # Cliente que fez o pedido
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)  # Cliente que fez o pedido
     criado_em = models.DateTimeField(auto_now_add=True)  # Data do pedido
-    atualizado_em = models.DateTimeField(auto_now=True)  # Última atualização
     finalizado = models.BooleanField(default=False)  # Status do pedido (em aberto ou finalizado)
+    endereco = models.CharField(max_length=200)
 
     def __str__(self):
         return self.id
