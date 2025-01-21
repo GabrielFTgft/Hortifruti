94c2713c869194aa644f8197c23b6414297e9873
mardex1 <mardemcastro123@gmail.com>
2025-01-21 19:55:52 -0300

    Tailwind Incorporado

--- hortifruti/hortifruti/urls.py
+++ hortifruti/hortifruti/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('produtos/', include('produtos.urls')),
     path('admin/', admin.site.urls),
     path('pedido/', include('pedidos.urls')),
+    path("__reload__", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
