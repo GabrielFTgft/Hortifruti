a196e4053af4a581d26d2abda131f5b39496f205
Gabriel Furtado Teixeira <110139952+GabrielFTgft@users.noreply.github.com>
2025-01-22 10:38:57 -0300

    Update urls.py

--- hortifruti/usuarios/urls.py
+++ hortifruti/usuarios/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('cadastro', views.cadastro, name='cadastro'),
-    path('principal', views.principal, name='principal'), # Página principal após login
-    path('deslogar', views.deslogar, name='deslogar'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('principal/', views.principal, name='principal'), # Página principal após login
+    path('deslogar/', views.deslogar, name='deslogar'),
 ]
